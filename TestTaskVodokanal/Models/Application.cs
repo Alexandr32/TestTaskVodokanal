9e2340e67c0b74ab31a2fa29fc1cf5ff2ab40c45
Alexandr <alex3289@gmail.com>
2019-08-12 16:42:37 +0600

    Созданная модель данных

---
+++ TestTaskVodokanal/Models/Application.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTaskVodokanal.Models
+{
+    public enum Status
+    {
+        Open,
+        Close,
+        Return,
+        Completed
+    }
+
+    /// <summary>
+    /// Класс заявок
+    /// </summary>
+    public class Application
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ApplicationID { get; set; }
+
+        /// <summary>
+        /// Дата регестрации заявки
+        /// </summary>
+        public DateTime RegistrationDate { get; set; }
+
+        /// <summary>
+        /// Статус завки
+        /// </summary>
+        public Status Status { get; set; }
+
+        /// <summary>
+        /// История изменений
+        /// </summary>
+        public ICollection<History> ChangeHistory { get; set; }
+
+    }
+}
---
+++ TestTaskVodokanal/Models/History.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTaskVodokanal.Models
+{
+    public class History
+    {
+        public int HistoryID { get; set; }
+
+        public int ApplicationId { get; set; }
+
+        [Required]
+        /// <summary>
+        /// Дата регестрации заявки
+        /// </summary>
+        public DateTime RegistrationDate { get; set; }
+
+        [Required]
+        /// <summary>
+        /// Статус завки
+        /// </summary>
+        public Status Status { get; set; }
+
+        [Required]
+        /// <summary>
+        /// Комментарий
+        /// </summary>
+        public string Comment { get; set; }
+
+    }
+}

311275a0f6193e8e85824315a670f905fa8b932d
Alexandr <alex3289@gmail.com>
2019-08-12 23:28:12 +0600

    Реализован поиск по статусу

--- TestTaskVodokanal/Pages/ApplicationPages/Index.cshtml.cs
+++ TestTaskVodokanal/Pages/ApplicationPages/Index.cshtml.cs
@@ -15,15 +15,37 @@ namespace TestTaskVodokanal.Pages.ApplicationPages
         public int ApplicationID{ get; set;}
         public ApplicationIndexData Application { get; set; }
 
+        /// <summary>
+        /// Свойство сортировки
+        /// </summary>
+        public string CurrentSort { get; set; }
+        /// <summary>
+        /// Свойства для сортировки имени
+        /// </summary>
+        public string StatusSort { get; set; }
+
+        /// <summary>
+        /// Выбранный статус для поиска
+        /// </summary>
+        //public Status SelectStatus { get; set; }
+
+
         private readonly TestTaskVodokanal.Models.TestTaskVodokanalContext _context;
 
         public IndexModel(TestTaskVodokanal.Models.TestTaskVodokanalContext context)
         {
             _context = context;
-        }
+        }       
 
-        public async Task OnGetAsync(int? id)
+        public async Task OnGetAsync(int? id, string sortOrder)
         {
+            CurrentSort = sortOrder;
+
+            // StatusSort используется как свойтво для фильтрации через asp-route-sortOrder
+            // Присваивается противоположное значение относительно текущего параметра сотояния 
+            // для дальнейшей сортировки при выборе необходимости сортировки
+            StatusSort = string.IsNullOrEmpty(sortOrder) ? "status_desc" : "";
+
             Application = new ApplicationIndexData
             {
                 Applications = await _context.Application
@@ -39,6 +61,27 @@ namespace TestTaskVodokanal.Pages.ApplicationPages
                 // Извлекаем заяки по id;
                 Application selectApplication = Application.Applications.Single(s => s.ApplicationID == id.Value);
                 Application.Historys = selectApplication.ChangeHistory.Where(s => s.ApplicationId == id.Value);
+            }
+
+            // Сортировка в зависимости от переданного параметра
+            switch (sortOrder)
+            {
+                // Выборка по статусу Порядок по убыванию
+                case "status_desc":
+                    Application.Applications = Application.Applications.OrderByDescending(s => s.Status);
+                    break;
+                // Выборка по дате Порядок по возростанию
+                //case "Date":
+                //    studentIQ = studentIQ.OrderBy(s => s.EnrollmentDate);
+                //    break;
+                //// Выборка по дате порядок по убыванию
+                //case "date_desc":
+                //    studentIQ = studentIQ.OrderByDescending(s => s.EnrollmentDate);
+                //    break;
+                // По умолчанию используется порядок сортировки по возрастанию.
+                default:
+                    Application.Applications = Application.Applications.OrderBy(s => s.Status);
+                    break;
             }
         }
     }
